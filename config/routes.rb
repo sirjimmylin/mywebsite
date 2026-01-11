65899978508a2a41b71b6a3be2aceae05b08c583
Jimmy Lin <sirjimmylin@duck.com>
2026-01-11 17:54:39 -0600

    Create Home, Projects, Blog, About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "pages/home"
+  get "pages/about"
+  # Set the homepage to the 'home' action in the 'Pages' controller
+  root 'pages#home'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Static pages
+  get 'about', to: 'pages#about'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Dynamic resources (Projects and Blog)
+  resources :projects
+  resources :posts
 end
